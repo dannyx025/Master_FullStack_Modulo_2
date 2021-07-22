8ab7adfd3afa46f1870ae418ac45da23f097f0ed
Danny <dannyx_1993@hotmail.com>
2021-07-21 20:30:51 -0600

    Clase Usuario creada

---
+++ claseUsuario.js
@@ -0,0 +1,14 @@
+class Usuario{
+
+    constructor(nombre,apellido1,apellido2,residencia){
+
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.residencia = residencia;
+
+    }
+
+
+
+}
